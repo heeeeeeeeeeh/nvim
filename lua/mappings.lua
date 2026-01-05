47b90f76dda6308f0b2e20e0ce5c29cedb1c8b60
Silvestre Espinoza <90989842+heeeeeeeeeeh@users.noreply.github.com>
2026-01-05 07:35:28 -0600

    feat(keybind): add git restore mapping to restore current file Adding it this patchset so i dont get lazy later

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,19 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+-- restore current file
+map("n", "<leader>grf", function ()
+  if vim.fn.input("restore current file current file (y/n): ") == "y" then
+  local file = vim.fn.expand("%")
+  if not file then
+      return vim.notify("no file in buffer")
+  end
+
+  vim.cmd(string.format([[ 
+    silent !git restore "%s"
+    silent !e
+  ]],file))
+  end
+end)
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
