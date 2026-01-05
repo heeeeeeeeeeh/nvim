d314a190039892ad8ee7d141d2910430385dd75d
Silvestre Espinoza <90989842+heeeeeeeeeeh@users.noreply.github.com>
2026-01-05 09:45:37 -0600

    fix(plugins): Change plugin specs that have config to opts. I [saw](https://github.com/folke/lazy.nvim/discussions/1185) that opts and config are the only diff being that you can def multiple specs with opts. That seems that like something i would do in the future so i should use opts from now on

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,7 +8,7 @@ return {
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
-    config = function()
+    opts = function()
       require "configs.lspconfig"
     end,
   },
