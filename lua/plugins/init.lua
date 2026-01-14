f297d6b04825ed5594ab9cff6fe58e0ff15494dc
Silvestre Espinoza <90989842+heeeeeeeeeeh@users.noreply.github.com>
2026-01-14 17:37:10 -0600

    fix(plugins): Moved some plugin configs to config dir

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,17 +1,8 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+  require("configs.conform"),
 
   -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    opts = function()
-      require "configs.lspconfig"
-    end,
-  },
+  require("configs.lspconfig")
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
